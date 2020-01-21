30952d2aec32c66317fa22fb824d08144bc3839b
LightBell <rhkswhdwkd@naver.com>
2020-01-21 21:12:25 +0900

    Delete prac_crawl.py

--- craw/prac_crawl.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-browser = webdriver.Chrome("./chromedriver")
-# let the browser wait for some time.
-time.sleep(3)
-# navigate to the Web URL
-browser.get("http://www.kpu.ac.kr/front/boardlist.do?bbsConfigFK=1&siteGubun=14&menuGubun=1#")
-print(browser)
